2ba001eeb4cd00cfa03ca26dddae58725ca1748e
dogstarchampion <btatkerson@gmail.com>
2015-02-24 02:33:39 -0500

    Made core_constants so it was inheritable by other classes easily and renamed methods so they wouldn't come up in auto-complete

--- core/__core_constants.py
+++ core/__core_constants.py
@@ -1,17 +1,48 @@
 '''
-Name: __core_constants.py
+Name: _core_constants.py
 
 Description:
 	This class stores the common-core constants that will be used in-game accross various classes
 	For instance, the creature in-game class "BARBARIAN" is used for things like maybe weapon requirements,
 	custom dialogs, level progression system, etc. Instead of the name "BARBARIAN" applying only to
-	core_creature files (which, the creatures would be "barbarians" in-game), the constants for them
+	core__creature files (which, the creatures would be "barbarians" in-game), the constants for them
 	can be used universally.
 '''
 
 
-class __core_constants():
-	class _creatureClass():
+class core_constants():
+
+	class _core_const_ability():
+		'''
+		Used as a class to hold constants for creature abilities
+		'''
+		def __init__(self):
+			self.STR=self.STRENGTH='str'
+			self.INT=self.INTELLIGENCE='int'
+			self.CON=self.CONSTITUTION='con'
+			self.WIS=self.WISDOM='wis'
+			self.DEX=self.DEXTERITY='dex'
+			self.CHR=self.CHARISMA='chr'
+
+	class _core_const_baseAttackBonus():
+		'''
+		Used as a class to hold constants for base attack bonuses
+		'''
+		def __init__(self):
+			self.GOOD = 'good' # Returns the constant for 'good' attack bonuses
+			self.AVERAGE = self.AVRG = 'average' # Returns the constant for 'average' attack bonuses
+			self.POOR = 'poor' # Returns the constant for 'poor' attack bonuses
+			self.MONK = 'monk' # Returns the constant for 'monk' attack bonuses
+
+	class _core_const_baseSaveBonus():
+		'''
+		Used as a class to hold constants for base save bonuses
+		'''
+		def __init__(self):
+			self.GOOD = 'good' # Returns the constant for 'good' save bonuses
+			self.POOR = 'poor' # Returns the constant for 'poor' save bonuses
+
+	class _core_const_creatureClass():
 		'''
 		Used to provide constants for valid creature classes
 		'''
@@ -27,21 +58,21 @@ class __core_constants():
 			self.RANGER = self.RGR = 'rgr'
 			self.ROGUE = self.ROG = 'rog'
 			self.SORCERER = self.SOR = 'sor'
-			
-
-	class _ability():
+	
+	class _core_const_creatureRace():
 		'''
-		Used as a class to hold constants for creature abilities
+		Used for holding the core races of the game
 		'''
 		def __init__(self):
-			self.STR=self.STRENGTH='str'
-			self.INT=self.INTELLIGENCE='int'
-			self.CON=self.CONSTITUTION='con'
-			self.WIS=self.WISDOM='wis'
-			self.DEX=self.DEXTERITY='dex'
-			self.CHR=self.CHARISMA='chr'
+			self.HUMAN = 'human'
+			self.DWARF = 'dwarf'
+			self.ELF = 'elf'
+			self.HALFELF = 'halfelf'
+			self.HALFORC = 'halforc'
+			self.HALFLING = 'halfling'
 
-	class _savingThrow():
+			
+	class _core_const_savingThrow():
 		'''
 		Used as a class to hold constants for saving throws
 		'''
@@ -50,27 +81,10 @@ class __core_constants():
 			self.REF=self.REFLEX='ref' # Returns constant for reflex saving throw
 			self.WIL=self.WILL='wil' # Returns constant for will saving throw
 
-	class _baseSaveBonus():
-		'''
-		Used as a class to hold constants for base save bonuses
-		'''
-		def __init__(self):
-			self.GOOD = 'good' # Returns the constant for 'good' save bonuses
-			self.POOR = 'poor' # Returns the constant for 'poor' save bonuses
-
-	class _baseAttackBonus():
-		'''
-		Used as a class to hold constants for base attack bonuses
-		'''
-		def __init__(self):
-			self.GOOD = 'good' # Returns the constant for 'good' attack bonuses
-			self.AVERAGE = self.AVRG = 'average' # Returns the constant for 'average' attack bonuses
-			self.POOR = 'poor' # Returns the constant for 'poor' attack bonuses
-			self.MONK = 'monk' # Returns the constant for 'monk' attack bonuses
-
 	def __init__(self):
-		self.CREATURECLASS = self.creature_Class()
-		self.ABILITY = self._ability()
-		self.BASEATTACKBONUS = self._baseAttackBonus()
-		self.BASESAVEBONUS = self._baseSaveBonus()
-		self.SAVINGTHROW = self._savingThrow()
+		self.ABILITY = self._core_const_ability()
+		self.BASEATTACKBONUS = self._core_const_baseAttackBonus()
+		self.BASESAVEBONUS = self._core_const_baseSaveBonus()
+		self.CREATURECLASS = self._core_const_creatureClass()
+		self.CREATURERACE = self._core_const_creatureRace()
+		self.SAVINGTHROW = self._core_const_savingThrow()
