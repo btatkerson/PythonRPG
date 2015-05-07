e44b22b0c564a89742056381d51fecd6e180ef18
dogstarchampion <btatkerson@gmail.com>
2015-05-07 01:51:51 -0400

    Fixed core constants to hold the long-names and added verification

--- core/__core_constants.py
+++ core/__core_constants.py
@@ -9,14 +9,46 @@ Description:
     can be used universally.
 '''
 
+class index_reader:
+    '''
+    Used for reading common data in the constant classes
+    ''' 
+
+    def __init__(self):
+        None
+
+    def is_real(self,val=None):
+        if not val:
+            return False,False
+        else:
+            if type(val) == int:
+                return True, dict(zip(range(1,len(self.INDEX)+1),self.INDEX))[val]
+            else:
+                if val in self.INDEX:
+                    return True, val
+            return False,False
+    
+    def verify(self,val=None):
+        real,value = self.is_real(val)
+        if real:
+            return value
+        else:
+            return False
+    
+    def get_long_name(self,val=None):
+        constant = self.verify(val)
+        return dict(zip(self.INDEX,self.INDEX_LONG))[constant]
+
+
 
 class core_constants():
 
-    class _core_const_ability():
+    class _core_const_ability(index_reader):
         '''
         Used as a class to hold constants for creature abilities
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.STR=self.STRENGTH='str'
                 self.INT=self.INTELLIGENCE='int'
                 self.CON=self.CONSTITUTION='con'
@@ -24,29 +56,41 @@ class core_constants():
                 self.DEX=self.DEXTERITY='dex'
                 self.CHR=self.CHARISMA='chr'
 
-    class _core_const_baseAttackBonus():
+                self.INDEX=[self.STR,self.INT,self.CON,self.WIS,self.DEX,self.CHR]
+                self.INDEX_LONG=['strength','intelligence','constitution','wisdom','dexterity','charisma']
+
+    class _core_const_baseAttackBonus(index_reader):
         '''
         Used as a class to hold constants for base attack bonuses
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.GOOD = 'good' # Returns the constant for 'good' attack bonuses
-                self.AVERAGE = self.AVRG = 'average' # Returns the constant for 'average' attack bonuses
+                self.AVERAGE = self.AVRG = 'avrg' # Returns the constant for 'average' attack bonuses
                 self.POOR = 'poor' # Returns the constant for 'poor' attack bonuses
                 self.MONK = 'monk' # Returns the constant for 'monk' attack bonuses
 
-    class _core_const_baseSaveBonus():
+                self.INDEX = [self.POOR,self.AVERAGE,self.GOOD,self.MONK]
+                self.INDEX_LONG=['poor','average','good','monk']
+
+    class _core_const_baseSaveBonus(index_reader):
         '''
         Used as a class to hold constants for base save bonuses
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.GOOD = 'good' # Returns the constant for 'good' save bonuses
                 self.POOR = 'poor' # Returns the constant for 'poor' save bonuses
 
-    class _core_const_creatureClass():
+                self.INDEX = [self.POOR,self.GOOD]
+                self.INDEX_LONG = ['poor','good']
+
+    class _core_const_creatureClass(index_reader):
         '''
         Used to provide constants for valid creature classes
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.UNIQUE = self.UNI = 'uni'
                 self.BARBARIAN = self.BBN = 'bbn'
                 self.BARD = self.BRD = 'brd'
@@ -59,52 +103,72 @@ class core_constants():
                 self.ROGUE = self.ROG = 'rog'
                 self.SORCERER = self.SOR = 'sor'
                 self.WIZARD = self.WIZ = 'wiz'
+
+                self.INDEX = [self.UNI,self.BBN,self.BRD,self.CLR,self.DRD,self.FTR,self.MNK,self.PLD,self.RGR,self.ROG,self.SOR,self.WIZ]
+                self.INDEX_LONG = ["unique","barbarian","bard","cleric","druid","fighter","monk","paladin","ranger","rogue","sorcerer","wizard"]
     
-    class _core_const_creatureRace():
+    class _core_const_creatureRace(index_reader):
         '''
         Used for holding the core races of the game
         '''
         def __init__(self):
-                self.UNIQUE = 'unique'
-                self.ABERRATION = 'aberration'
-                self.ANIMAL = 'animal'
-                self.BEAST = 'beast'
-                self.CONSTRUCT = 'construct'
-                self.DRAGON = 'dragon'
-                self.DWARF = 'dwarf'
-                self.ELEMENTAL = 'elemental'
+                index_reader.__init__(self)
+                self.UNI = self.UNIQUE = 'unique'
+                self.ABR = self.ABERRATION = 'aberration'
+                self.ANI = self.ANIMAL = 'animal'
+                self.BST = self.BEAST = 'beast'
+                self.CON = self.CONSTRUCT = 'construct'
+                self.DRG = self.DRAGON = 'dragon'
+                self.DWF = self.DWARF = 'dwarf'
+                self.ELM = self.ELEMENTAL = 'elemental'
                 self.ELF = 'elf'
                 self.FEY = 'fey'
-                self.GIANT = 'giant'
-                self.GOBLINOID = 'goblinoid'
-                self.HALFELF = 'halfelf'
-                self.HALFORC = 'halforc'
-                self.HALFLING = 'halfling'
-                self.HUMAN = 'human'
-                self.MAGICAL_BEAST = 'magical_beast'
-                self.OOZE = 'ooze'
+                self.GNT = self.GIANT = 'giant'
+                self.GOB = self.GOBLINOID = 'goblinoid'
+                self.HFE = self.HALFELF = 'halfelf'
+                self.HFO = self.HALFORC = 'halforc'
+                self.HFL = self.HALFLING = 'halfling'
+                self.HUM = self.HUMAN = 'human'
+                self.MAG = self.MAGICAL_BEAST = 'magical_beast'
+                self.OOZ = self.OOZE = 'ooze'
                 self.ORC = 'orc'
-                self.OUTSIDER = 'outsider'
-                self.REPTILIAN = 'reptilian'
-                self.SHAPECHANGER = 'shapechanger'
-                self.UNDEAD = 'undead'
-                self.VERMIN = 'vermin'
+                self.OUT = self.OUTSIDER = 'outsider'
+                self.REP = self.REPTILIAN = 'reptilian'
+                self.SHP = self.SHAPECHANGER = 'shapechanger'
+                self.UND = self.UNDEAD = 'undead'
+                self.VRM = self.VERMIN = 'vermin'
 
+                self.INDEX = [self.UNI,self.ABR,self.ANI,self.BST,self.CON,
+                              self.DRG,self.DWF,self.ELM,self.ELF,self.FEY,
+                              self.GNT,self.GOB,self.HFE,self.HFO,self.HFL,
+                              self.HUM,self.MAG,self.OOZ,self.ORC,self.OUT,
+                              self.REP,self.SHP,self.UND,self.VRM]
+
+                self.INDEX_LONG = ['unique','aberration','animal','beast','construct',
+                                   'dragon','dwarf','elemental','elf','fey',
+                                   'giant','goblin','half-elf','half-orc','half-ling',
+                                   'human','magical beast','ooze','orc','outsider',
+                                   'reptilian','shapechanger','undead','vermin']
                 
-    class _core_const_savingThrow():
+    class _core_const_savingThrow(index_reader):
         '''
         Used as a class to hold constants for saving throws
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.FOR=self.FORTITUDE='for' # Returns constant for fortitude saving throw
                 self.REF=self.REFLEX='ref' # Returns constant for reflex saving throw
                 self.WIL=self.WILL='wil' # Returns constant for will saving throw
 
-    class _core_const_skill():
+                self.INDEX = [self.FOR,self.REF,self.WIL]
+                self.INDEX_LONG = ['fortitude','reflex','will']
+
+    class _core_const_skill(index_reader):
         '''
         Used as a class to hold possible skills for a creature
         '''
         def __init__(self):
+                index_reader.__init__(self)
                 self.UNI = self.UNIQUE = 'uni'
                 self.ANI = self.ANIMAL_EMPATHY = 'ani'
                 self.APR = self.APPRAISE = 'apr'
@@ -134,6 +198,20 @@ class core_constants():
                 self.TNT = self.TAUNT = 'tnt'
                 self.TBL = self.TUMBLE = 'tbl'
                 self.UMD = self.USE_MAGIC_DEVICE = 'umd'
+
+                self.INDEX = [self.UNI,self.ANI,self.APR,self.BLF,self.CNC,
+                              self.CAR,self.CTR,self.CWE,self.DTR,self.DIS,
+                              self.HEL,self.HID,self.ITM,self.LIS,self.LOR,
+                              self.MOV,self.OPL,self.PRY,self.PFM,self.PSD,
+                              self.PIC,self.RID,self.SRC,self.STR,self.SPL,
+                              self.SPT,self.TNT,self.TBL,self.UMD]
+
+                self.INDEX_LONG = ['unique', 'animal empathy', 'appraise', 'bluff', 'concentration',
+                                   'craft armor', 'craft trap', 'craft weapon', 'disable trap','discipline',
+                                   'heal', 'hide', 'intimidate', 'listen', 'lore',
+                                   'move silently', 'open lock', 'parry', 'perform', 'persuade',
+                                   'pick pocket', 'ride', 'search', 'set trap', 'spellcraft',
+                                   'spot', 'taunt', 'tumble', 'use magic device']
 
 
     def __init__(self):
--- test_battle.py
+++ test_battle.py
@@ -6,7 +6,7 @@ import os
 import creature as cre
 
 a = cc()
-pc = cre.creature(True,"Ben",a.CREATURECLASS.MONK,a.CREATURERACE.HUMAN,None,23,98,20,4,None,15,None,14,10,7,13,11,3,True)
+pc = cre.creature(True,"Frodo",a.CREATURECLASS.MONK,a.CREATURERACE.HUMAN,None,23,98,20,4,None,15,None,14,10,7,13,11,3,True)
 npc = cre.creature(False,"Goblin",a.CREATURECLASS.FTR,a.CREATURERACE.GOBLINOID,None,20,40,15,2,None,10,None,12,6,5,10,10,7,True)
 
 print("\n\n---------------------------------------------------------------\n\n")
@@ -24,7 +24,10 @@ def attack(cre_a,cre_b):
         if not cre_b.is_alive():
             break
         hit_or_miss = i[1] >= cre_b.get_armor_class()
-        print(cre_a.get_name(),'rolls:',i[0],"|",i[1],">=" if hit_or_miss else "<",cre_b.get_armor_class(),"| HIT!" if hit_or_miss else "| MISS!")
+        if i[0] != 0:
+            print(cre_a.get_name(),'rolls:',i[0],"|",i[1],">=" if hit_or_miss else "<",cre_b.get_armor_class(),"| HIT!" if hit_or_miss else "| MISS!")
+        else:
+            print(cre_a.get_name(),"got a critical miss! No damage was done to", cre_b.get_name())
         if hit_or_miss or i[0] == 20:
             damage = 0
             if i[0] == 20:
@@ -53,13 +56,13 @@ while pc.is_alive() and npc.is_alive():
         heal(pc) 
     
     if not npc.is_alive():
+        input("\nPress Enter to continue")
         break
 
     print("\n")
 
     attack_or_heal = sum(npc.d100())
 
-    print(npc.get_current_hit_points(),npc.get_base_hit_points(),attack_or_heal,int(npc.get_current_hit_points()*100/npc.get_base_hit_points()))
     if npc.get_current_hit_points() is not npc.get_base_hit_points() and attack_or_heal >= int(npc.get_current_hit_points()*100/npc.get_base_hit_points()):
         heal(npc)
     else:
